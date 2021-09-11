8782867a265af79e6fee25253eaf9e8a3846e78f
José Oliveira <j.oliveira90@live.com>
2021-09-11 20:31:33 -0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-plantmanager react-native
+<p align="center">
+   <img src=".github/logo.png" alt="Plantmanager" width="280"/>
+</p>
+
+<p align="center">
+  
+ <img alt="Repository size" src="https://img.shields.io/github/repo-size/Olliveer/Olliveer?color=00C063">
+
+  <a aria-label="Last Commit" href="https://github.com/Olliveer/Olliveer-nlw/commits/master">
+    <img alt="Last commit on GitHub" src="https://img.shields.io/github/last-commit/Olliveer/plantmanager?color=00C063">
+  </a>
+  <a href="https://github.com/Olliveer/plantmanager/commits/master">
+    <img alt="GitHub last commit" src="https://img.shields.io/github/last-commit/Olliveer/plantmanager?color=00C063">
+  </a>
+  <img alt="License" src="https://img.shields.io/badge/license-MIT-00C063">
+</p>
+
+<b>PlantManager</b> é um aplicativo desenvolvido utilizando React Native e TypeScript durante a NLW#5 da [Rocketseat](https://github.com/Rocketseat). Esse projeto consiste em ajuda-lo a gerenciar suas plantihas.
+
+# :pushpin: Conteúdo
+
+- [Tecnologias](#computer-tecnologias)
+- [Executando](#construction_worker-executando)
+- [Autores](#computer-autores)
+- [Licença](#closed_book-licença)
+
+### Demonstração
+
+<div>
+  <img src=".github/banner1.gif" width="400" />
+  <img src=".github/banner2.gif" width="400" />
+</div>
+
+# :computer: Tecnologias
+
+Este projeto foi feito utilizando as seguintes tecnologias:
+
+- [React Native](https://reactnative.dev/)
+- [Expo](https://expo.io/)
+- [Typescript](https://www.typescriptlang.org/)
+- [Axios](https://github.com/axios/axios)
+- [date-fns](https://date-fns.org/)
+- [lottie-react-native](https://github.com/lottie-react-native/lottie-react-native)
+- [react-native-svg](https://github.com/react-native-svg/react-native-svg)
+- [react-native-iphone-x-helper](https://github.com/ptelad/react-native-iphone-x-helper)
+
+# :construction_worker: Executando
+
+## 1. Clone o Repositório
+
+```bash
+https://github.com/Olliveer/plantmanager.git
+```
+
+## 2. Instale as Dependencias
+
+```bash
+yarn
+```
+
+## 3. Acesse o arquivo `api` na pasta `src/services` e altere a `baseURL` para a sua
+
+```javascript
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://SUA_URL:3333",
+});
+
+export default api;
+```
+
+## 4. Inicie o Servidor através do JSON Sever
+
+```bash
+yarn server
+```
+
+> 💡 **Observação**
+>
+> Caso não tenha o **JSON Server** instalado ou não saiba o que é, [acesse aqui.](https://github.com/typicode/json-server#getting-started)
+
+## 5. Inicie o Expo
+
+```bash
+$ expo start
+
+$ yarn server
+```
+
+# :closed_book: Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
+---
+
+[![image](https://img.shields.io/badge/😎%20José%20Oliveira,%202021-LinkedIn-00C063?style=flat-square)](https://www.linkedin.com/in/joseooliveira/)
--- src/pages/MyPlants.tsx
+++ src/pages/MyPlants.tsx
@@ -1,38 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { formatDistance } from 'date-fns';
-import { pt } from 'date-fns/locale';
-import { Alert, FlatList, Image, StyleSheet, Text, View } from 'react-native';
-import waterdrop from '../assets/waterdrop.png';
-import { Header } from '../components/Header';
-import { Load } from '../components/Load';
-import { PlantCardSecondary } from '../components/PlantCardSecondary';
-import { loadPlant, PlantProps, removePlant } from '../libs/storage';
-import colors from '../styles/colors';
-import fonts from '../styles/fonts';
-
+import React, { useEffect, useState } from "react";
+import { formatDistance } from "date-fns";
+import { pt } from "date-fns/locale";
+import { Alert, FlatList, Image, StyleSheet, Text, View } from "react-native";
+import waterdrop from "../assets/waterdrop.png";
+import { Header } from "../components/Header";
+import { Load } from "../components/Load";
+import { PlantCardSecondary } from "../components/PlantCardSecondary";
+import { loadPlant, PlantProps, removePlant } from "../libs/storage";
+import colors from "../styles/colors";
+import fonts from "../styles/fonts";
 
 export function MyPlants() {
   const [myPlants, setMyPlants] = useState<PlantProps[]>([]);
   const [loading, setLoading] = useState(true);
   const [nextWatered, setNextWatered] = useState<string>();
 
-
   function handleRemove(plant: PlantProps) {
-    Alert.alert('Remover', `Deseja remover a ${plant.name}`, [
-      { text: 'Não 🙏', style: 'cancel' },
+    Alert.alert("Remover", `Deseja remover a ${plant.name}`, [
+      { text: "Não 🙏", style: "cancel" },
       {
-        text: 'Sim 😢', onPress: async () => {
+        text: "Sim 😢",
+        onPress: async () => {
           try {
             removePlant(plant.id);
             setMyPlants((oldData) =>
               oldData.filter((item) => item.id !== plant.id)
             );
           } catch (error) {
-            Alert.alert('Não foi possível remover 😢');
+            Alert.alert("Não foi possível remover 😢");
           }
-        }
-      }
-
+        },
+      },
     ]);
   }
 
@@ -44,22 +42,21 @@ export function MyPlants() {
         new Date(plantsStoraged[0].dateTimeNotification).getTime(),
         new Date().getTime(),
         {
-          locale: pt
+          locale: pt,
         }
-      )
+      );
 
-      setNextWatered(`Regue sua ${plantsStoraged[0].name} daqui a ${nextTime}`)
+      setNextWatered(`Regue sua ${plantsStoraged[0].name} daqui a ${nextTime}`);
       setMyPlants(plantsStoraged);
       setLoading(false);
     }
 
-    loadStorageData()
-  }, [])
+    loadStorageData();
+  }, []);
 
   if (loading) {
-    return <Load />
+    return <Load />;
   }
-
 
   return (
     <View style={styles.container}>
@@ -71,46 +68,42 @@ export function MyPlants() {
       </View>
 
       <View style={styles.plants}>
-        <Text style={styles.plantsTitle}>
-          Próximas regadas
-        </Text>
+        <Text style={styles.plantsTitle}>Próximas regadas</Text>
 
         <FlatList
           data={myPlants}
-          keyExtractor={item => String(item.id)}
+          keyExtractor={(item) => String(item.id)}
           renderItem={({ item }) => (
-
             <PlantCardSecondary
               data={item}
               handleRemove={() => handleRemove(item)}
             />
           )}
           showsVerticalScrollIndicator={false}
-        // contentContainerStyle={{ flex: 1 }}
+          // contentContainerStyle={{ flex: 1 }}
         />
       </View>
     </View>
-
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    alignItems: "center",
+    justifyContent: "space-between",
     paddingHorizontal: 30,
     paddingTop: 50,
-    backgroundColor: colors.background
+    backgroundColor: colors.background,
   },
   spotlight: {
     backgroundColor: colors.blue_light,
     paddingHorizontal: 20,
     borderRadius: 20,
     height: 110,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   spotlightImage: {
     width: 60,
@@ -123,12 +116,12 @@ const styles = StyleSheet.create({
   },
   plants: {
     flex: 1,
-    width: '100%'
+    width: "100%",
   },
   plantsTitle: {
     fontSize: 24,
     fontFamily: fonts.heading,
     color: colors.heading,
-    marginVertical: 20
-  }
+    marginVertical: 20,
+  },
 });
